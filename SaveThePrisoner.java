b158cc9c7ab725af11934f4e01a30debc54dc9e6
Kanwar Bhajneek <kb.kb.1990@gmail.com>
2016-07-16 17:26:41 -0400

    SaveThePrisoner!

---
+++ SaveThePrisoner.java
@@ -0,0 +1,17 @@
+//Save the Prisoner !
+import java.util.*;
+
+public class SaveThePrisoner {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			int S = sc.nextInt();
+			System.out.println((S + M - 1) % N == 0 ? N : (S + M - 1) % N);
+		}
+
+	}
+}
